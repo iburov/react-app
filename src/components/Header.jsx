1927a47dd693a8246dab4fe561caf9658e5cbfc5
iburov.ca <iburov.ca@gmail.com>
2019-10-22 11:26:26 -0400

    react-tut-13

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+        className="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="#profile">PROFILE</a>
+      </div>
+      <div>
+        <a href="#messages">MESSAGES</a>
+      </div>
+      <div>
+        <a href="#news">NEWS</a>
+      </div>
+      <div>
+        <a href="#music">MUSIC</a>
+      </div>
+      <div>
+        <a href="#settings">SETTINGS</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
