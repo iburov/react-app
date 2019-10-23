2efe8a26ec145f3d12c2a8ec48395b2ff9707e43
iburov.ca <iburov.ca@gmail.com>
2019-10-22 22:01:29 -0400

    react-tut-21-messenger-page-making

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -3,8 +3,21 @@ import c from "./Messenger.module.css";
 
 const Messenger = props => {
   return (
-    <div>
-      <span>Messenger</span>
+    <div className={c.dialogs}>
+      <div className={c.dialogs_items}>
+        <div className={c.dialog}>Ivan</div>
+        <div className={c.dialog + " " + c.active}>Marina</div>
+        <div className={c.dialog}>Maxim</div>
+        <div className={c.dialog}>Julia</div>
+      </div>
+      <div className={c.messages}>
+        <div className={c.message}>Hi!</div>
+        <div className={c.message}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+        <div className={c.message}>Photo</div>
+      </div>
     </div>
   );
 };
