bd68596cabfc44b12a81bc34a977f858d1c5208e
iburov.ca <iburov.ca@gmail.com>
2019-10-22 22:17:15 -0400

    react-tut-22-route-exact

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import c from "./Messenger.module.css";
+import { NavLink } from "react-router-dom";
 
 const Messenger = props => {
   return (
     <div className={c.dialogs}>
       <div className={c.dialogs_items}>
-        <div className={c.dialog}>Ivan</div>
-        <div className={c.dialog + " " + c.active}>Marina</div>
-        <div className={c.dialog}>Maxim</div>
-        <div className={c.dialog}>Julia</div>
+        <div className={c.dialog}>
+          <NavLink to="/messenger/1">Ivan</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/messenger/2">Marina</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/messenger/3">Maxim</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/messenger/4">Julia</NavLink>
+        </div>
       </div>
       <div className={c.messages}>
         <div className={c.message}>Hi!</div>
